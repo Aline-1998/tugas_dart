e28c58011a766843b02d621430922be5a37ab043
claraangella51 <claraangella51@gmail.com>
2026-05-07 09:38:42 +0700

    tugas2.dart

---
+++ tugas3.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+void main() {
+  //for bilangan ganjil
+  for (int i = 1; i <=20; i++){
+    if( i % 2 !=0){
+      print("bilangan ganjil $i");
+    }
+  }
+   //cetak karakter
+   for (int i = 0; i < 5; i++){
+    stdout.write('*');
+   }
+
+   //nama berulang
+   int j = 1;
+   while(j <= 4){
+    print("aline $j");
+    j++;
+   }
+   //perulangan dalam list
+   List<String> namahewan = [ 'kucing', 'monyet', 'kangguru', 'harimau'];
+
+   for (var hewanlucu in namahewan){
+    print("Nama Hewan Lucu $hewanlucu");
+   }
+
+    // List daftar belanja
+    List<String> daftarBelanja = ['beras', 'sayur', 'buah', 'daging'];
+
+    //menggunakan for loop untuk menampilkan daftar belanja
+    for (var kebutuhan in daftarBelanja ){
+      print("Daftar Kebutuhan $kebutuhan");
+    }
+    }
